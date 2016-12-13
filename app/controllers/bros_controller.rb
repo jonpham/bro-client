46765a13b62c19a56cf61d39bf99ffbe86cfc5d3
Jonathan Pham <jon.pham@gmail.com>
2016-12-12 20:39:31 -0800

    Add ability to update bros from web-client.

--- app/controllers/bros_controller.rb
+++ app/controllers/bros_controller.rb
@@ -9,7 +9,7 @@ class BrosController < ApplicationController
   end
 
   def create
-    Unirest.post("http://localhost:3001/api/v2/bros/",
+    response = Unirest.post("http://localhost:3001/api/v2/bros/",
       headers:{"Accept" => "application/json"},
       parameters:{
         name: params[:input_name],
@@ -18,9 +18,26 @@ class BrosController < ApplicationController
         job: params[:input_job]
       }
     )
-    redirect_to :index
+    redirect_to "/bros/#{response.body['id']}"
   end
 
   def new
+  end
+
+  def edit
+    @bro = Unirest.get("http://localhost:3001/api/v2/bros/#{params[:id]}.json").body
+  end
+
+  def update
+    response = Unirest.patch("http://localhost:3001/api/v2/bros/#{params[:id]}",
+      headers:{"Accept" => "application/json"},
+      parameters:{
+        name: params[:input_name],
+        bio:params[:input_bio],
+        fav_food:params[:input_fav_food],
+        job: params[:input_job]
+      }
+    )
+    redirect_to "/bros/#{response.body['id']}"
   end
 end
